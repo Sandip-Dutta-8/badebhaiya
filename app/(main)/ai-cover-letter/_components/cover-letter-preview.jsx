8795185f390b577f50e96997f93ab9b503d4cbdf
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2025-02-26 01:35:45 +0530

    fixing

--- app/(main)/ai-cover-letter/_components/cover-letter-preview.jsx
+++ app/(main)/ai-cover-letter/_components/cover-letter-preview.jsx
@@ -6,7 +6,7 @@ import MDEditor from "@uiw/react-md-editor";
 const CoverLetterPreview = ({ content }) => {
     return (
         <div className="py-4">
-            <MDEditor value={content} preview="edit" height={700} />
+            <MDEditor value={content} preview="preview" height={700} />
         </div>
     );
 };
